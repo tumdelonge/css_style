768da3133646217778005e54bfc7f4e1bd5c1df7
tumdelonge <tumdelonge@gmail.com>
2022-10-28 23:39:47 +0700

    homepage site with css

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template-1</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <link href='https://css.gg/instagram.css' rel='stylesheet'>
+    <link href='https://css.gg/twitter.css' rel='stylesheet'>
+    <link href='https://css.gg/mail.css' rel='stylesheet'>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo-nvgs"><a href=""><span class="material-symbols-outlined">
+                        home_app_logo
+                    </span></a></div>
+            <div class="btn-nvgs">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Service</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main">
+            <div class="text-box">
+                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum nulla iure praesentium modi. Maiores consequuntur obcaecati nostrum doloribus, amet minus!</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nesciunt magni natus magnam debitis! Nihil.</p>
+                <a class="btn-box" href="">Lorem, ipsum.</a>
+            </div>
+            <div class="service-box">
+                <a href="" class="layanan satu">
+                    <span class="material-symbols-outlined">
+                        join_left
+                    </span>
+                    <h4>Layanan satu</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magnam quae culpa neque tenetur quos officiis corporis id illum et.</p>
+                    <div class="view-box">View</div>
+                </a>
+                <a href="" class="layanan dua">
+                    <span class="material-symbols-outlined">
+                        join_left
+                    </span>
+                    <h4>Layanan dua</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ducimus facere, sapiente fugiat error quae. Quas praesentium illum eos ex?</p>
+                    <div class="view-box">View</div>
+                </a>
+                <a href="" class="layanan tiga">
+                    <span class="material-symbols-outlined">
+                        join_left
+                    </span>
+                    <h4>Layanan tiga</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cumque enim reiciendis aliquam veniam deserunt consequuntur placeat animi rem perspiciatis.</p>
+                    <div class="view-box">View</div>
+
+                </a>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="copy">
+                <p>copyright @2022. all righ reserved</p>
+            </div>
+            <div class="soc-media">
+                <div class="icon instagram">
+                    <a href="" class="gg-instagram"></a>
+                </div>
+                <div class="soc label"><a class="instagram" href="">@loremipsum</a></div>
+                <div class="icon twitter">
+                    <a href="" class="gg-twitter"></a>
+                </div>
+                <div class="soc label"><a class="twitter" href="">@loremipsum</a></div>
+                <div class="icon gmail">
+                    <a href="" class="gg-mail"></a>
+                </div>
+                <div class="soc label"><a class="gmail" href="">loremipsum@gmail.com</a></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
